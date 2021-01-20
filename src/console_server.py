14312e09afb5cfaa7d4992ea18c11272973154c9
Matt Lewis <matt@mplewis.com>
2021-01-19 17:02:36 -0700

    wip

--- src/console_server.py
+++ src/console_server.py
@@ -1,13 +1,21 @@
 import websockets
 from werkzeug.debug.console import Console
 
-async def wss(websocket, path):
-    c = Console()
-    async for cmd in websocket:
-        print(f"< {cmd.rstrip()}")
-        result = c.eval(cmd).replace('\n', '<br>')
-        await websocket.send(result)
-        print(f"> {result}")
+def wss(stop):
+    async def s(websocket, path):
+        c = Console()
+        try:
+            async for cmd in websocket:
+                print(f"< {cmd.rstrip()}")
+                result = c.eval(cmd).replace('\n', '<br>')
+                await websocket.send(result)
+                print(f"> {result}")
+        finally:
+            print('Disconnected')
+            stop()
+    return s
 
 def console_server(host, port):
-    return websockets.serve(wss, host, port)
+    def stop():
+        print('OK, stopped')
+    return websockets.serve(wss(stop), host, port)
--- src/main.py
+++ src/main.py
@@ -6,10 +6,12 @@ HOST = 'localhost'
 WS_PORT = 8765
 HTTP_PORT = 8888
 
-asyncio.get_event_loop().run_until_complete(console_server(HOST, WS_PORT))
+loop = asyncio.get_event_loop()
+
+loop.run_until_complete(console_server(HOST, WS_PORT))
 print(f'Listening for WS connections on ws://{HOST}:{WS_PORT}')
 
-asyncio.get_event_loop().run_until_complete(static_server(HOST, HTTP_PORT))
+loop.run_until_complete(static_server(HOST, HTTP_PORT))
 print(f'Listening for HTTP connections on http://{HOST}:{HTTP_PORT}')
 
-asyncio.get_event_loop().run_forever()
+loop.run_forever()
